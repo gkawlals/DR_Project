505f13648eca0e8e1ac0edad406babc693ba169c
hamjimin <hua4187@naver.com>
2021-03-15 18:17:54 +0900

    solo project

---
+++ SpringPRJ/src/poly/dto/MailDTO.java
@@ -0,0 +1,29 @@
+package poly.dto;
+
+public class MailDTO {
+
+	private String toMail;
+	private String title;
+	private String content;
+	
+	public String getToMail() {
+		return toMail;
+	}
+	public void setToMail(String toMail) {
+		this.toMail = toMail;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	
+}
